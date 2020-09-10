eb9908766acdc974a3c27ba7c95d19e998aeb567
CodyMiner <cminer145@gmail.com>
2020-09-10 15:06:59 -0400

    Implement "Clear" functionality

--- Calculator/src/ButtonClickListener.java
+++ Calculator/src/ButtonClickListener.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.lang.reflect.Method;
 
 public class ButtonClickListener implements ActionListener {
     Operations operation = new Operations();
@@ -64,7 +62,12 @@ public class ButtonClickListener implements ActionListener {
 
         }
         else if (command.equals("÷")) {
-
+            if(!operationClicked && !firstBinNum.equals("")) {
+                currentOperation = "÷";
+                operationClicked = true;
+                currentEquation = firstBinNum + " ÷ ";
+                binLabel.setText(currentEquation);
+            }
 
         }
         else if (command.equals("x\u00B2")) {
@@ -77,10 +80,22 @@ public class ButtonClickListener implements ActionListener {
         else if (command.equals("=")) {
             result = getOperation(currentOperation);
             currentEquation = currentEquation + secondBinNum;
-            binLabel.setText(currentEquation + " = "+ operation.decimalToBinary(result));
+            if(!binLabel.getText().equals("Operation Error")) {
+                binLabel.setText(currentEquation + " = " + operation.decimalToBinary(result));
+            }
+            //TODO: should set operationClicked to false?
         }
         else if (command.equals("Switch")) {
 
+        }
+        else if (command.equals("Clear")) {
+            firstBinNum = "";
+            secondBinNum = "";
+            currentEquation = "";
+            currentOperation = "";
+            result = 0;
+            binLabel.setText("");
+            operationClicked = false;
         }
     }
 
@@ -97,6 +112,13 @@ public class ButtonClickListener implements ActionListener {
             return operation.doAddition(firstDecNum,secondDecNum);
         }else if(op.equals("-")){
             return operation.doSubtraction(firstDecNum,secondDecNum);
+        }else if(op.equals("÷")) {
+            try {
+                return operation.doDivide(firstDecNum, secondDecNum);
+                }catch(IllegalArgumentException e){
+                binLabel.setText("Operation Error");
+                return 0;
+            }
         }else{
             return firstDecNum;
         }
