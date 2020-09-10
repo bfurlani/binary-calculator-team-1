3dff039bfb6f1785c851f5b9742c4a5d38bb6ee9
bfurlani <benjaminfurlani.1112@gmail.com>
2020-09-09 21:07:04 -0400

    Added doSquareRoot(int num); and doSquare(int num); to Operations.java

--- Calculator/src/Operations.java
+++ Calculator/src/Operations.java
@@ -1,3 +1,13 @@
 public class Operations {
 
+
+    public double doSquareRoot(int num){
+        double root = Math.sqrt(num);
+        return root;
+    }
+
+    public int doSquare(int num){
+        int square = num*num;
+        return square;
+    }
 }
